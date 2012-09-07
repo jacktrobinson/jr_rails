89a655f539f29a4c6f55eae2550b5ab6abc2ebca
Jack Robinson <jrobins@wharton.upenn.edu>
2012-09-07 16:36:15 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JrRails::Application.config.secret_token = 'e373b4c4be90b3e3e530246c15adc4dfaee7eeb5dead59c75cecb4bcce757f2f7fc6adce17208103fd2b46af9a0e36bed176e36d65c277dde2f5f9f215eb6a3c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+JrRails::Application.config.session_store :cookie_store, key: '_jr_rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# JrRails::Application.config.session_store :active_record_store
