5f59e2b06b96444fa639886c9fae9c32d6686735
Jack Robinson <jrobins@wharton.upenn.edu>
2012-10-24 09:40:34 -0400

    another

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -2,10 +2,10 @@ require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase  
  
-  test "requires a name" do
+  test "requires a title" do
     debugger;1
     subject.should_not be_valid
-    subject.name = Insert_Name
+    subject.title = "Insert_Name"
     subject.should be_valid
   end
 
