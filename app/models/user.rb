9bb4007a76219354002da87ee31400ac0c5480c8
Jack Robinson <jrobins@wharton.upenn.edu>
2012-11-18 23:52:41 -0500

    heroku fix, precompile true

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,6 @@ class User < ActiveRecord::Base
   belongs_to :user
   attr_accessible :email, :name, :occupation, :user
   
-  #validates :name,  :uniqueness => true,
-  #                  :presence => true
   validates :email, :presence => true
 
 end
